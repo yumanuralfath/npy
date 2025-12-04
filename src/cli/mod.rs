8d20f65d21bef21ee3c21e657722891e36a70ebb
yumanuralfath <yumanuralfath2@gmaiil.com>
2025-12-04 08:07:52 +0700

    feat(cli): add cli init

---
+++ src/cli/mod.rs
@@ -0,0 +1,16 @@
+// pub mod  args;
+
+// use  args::Cli;
+// use clap::Parser;
+
+// pub fn run() -> Result<(), Box<dyn std::error::Error>> {
+//     let cli = Cli::parse();
+    
+//     let rt = tokio::runtime::Runtime::new()?;
+
+//     rt.block_on(async {
+//         Ok::<(), Box<dyn std::error::Error>>
+//     })?;
+    
+//     Ok(())
+// }
