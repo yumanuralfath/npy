9b38873bc45dafea7d1f1725a7f63a6a3d42e9b8
yumanuralfath <yumanuralfath2@gmail.com>
2025-12-07 09:47:49 +0700

    feat(verbose): add verbose function

--- src/tool/mod.rs
+++ src/tool/mod.rs
@@ -1,1 +1,12 @@
+use clap::Parser;
+
+use crate::cli::args::Cli;
+
 pub mod data;
+pub mod panther;
+
+pub fn verbose<T: std::fmt::Debug>(value: T) {
+    if Cli::parse().verbose {
+        println!("{:?}", value)
+    }
+}
